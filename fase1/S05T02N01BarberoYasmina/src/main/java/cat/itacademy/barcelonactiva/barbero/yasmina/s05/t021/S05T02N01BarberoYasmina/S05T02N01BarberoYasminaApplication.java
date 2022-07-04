87eff2cb1a598870249b17f6d3cfeffad57c6218
YasMinguet <100420073+YasMinguet@users.noreply.github.com>
2022-07-04 12:04:19 +0200

    fase1_complete

---
+++ fase1/S05T02N01BarberoYasmina/src/main/java/cat/itacademy/barcelonactiva/barbero/yasmina/s05/t021/S05T02N01BarberoYasmina/S05T02N01BarberoYasminaApplication.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.barbero.yasmina.s05.t021.S05T02N01BarberoYasmina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S05T02N01BarberoYasminaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S05T02N01BarberoYasminaApplication.class, args);
+	}
+
+}
---
+++ fase1/S05T02N01BarberoYasmina/src/test/java/cat/itacademy/barcelonactiva/barbero/yasmina/s05/t021/S05T02N01BarberoYasmina/S05T02N01BarberoYasminaApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.barbero.yasmina.s05.t021.S05T02N01BarberoYasmina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S05T02N01BarberoYasminaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
